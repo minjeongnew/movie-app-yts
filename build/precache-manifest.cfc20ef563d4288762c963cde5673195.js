fa1d3a80ff744488804b2dc47d8bca294b311400
minjeongnew <20171623sw@kookmin.ac.kr>
2021-06-05 18:39:58 +0900

    feat:
    Signed-off-by: minjeongnew <20171623sw@kookmin.ac.kr>

---
+++ build/precache-manifest.cfc20ef563d4288762c963cde5673195.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94988cd8ba8f1ad186d38ce530b3fb14",
+    "url": "./index.html"
+  },
+  {
+    "revision": "addc662b65c456815e79",
+    "url": "./static/css/main.b06ce88f.chunk.css"
+  },
+  {
+    "revision": "af1118f7b0696ab5c120",
+    "url": "./static/js/2.0ad94257.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0ad94257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "addc662b65c456815e79",
+    "url": "./static/js/main.8e1fb1a8.chunk.js"
+  },
+  {
+    "revision": "55373b66fa08ec57a235",
+    "url": "./static/js/runtime-main.d6af4fba.js"
+  }
+]);
