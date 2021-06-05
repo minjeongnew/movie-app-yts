c09f83b1890d6a3bdbfa3fa4af8e1c17c8aa385a
minjeongnew <20171623sw@kookmin.ac.kr>
2021-06-06 00:17:36 +0900

    Modify get api url
    Signed-off-by: minjeongnew <20171623sw@kookmin.ac.kr>

--- src/api.js
+++ src/api.js
@@ -12,9 +12,9 @@ export const getSearchMovieAPI = {
     let sb = search.split(" ")[0]
     let limit = search.split(" ")[1]
     if (sb === "like") {
-      return api.get(`/yts/api/v2/list_movies.json?sort_by=like_count&order_by=desc&limit=${limit}`)
+      return api.get(`/api/v2/list_movies.json?sort_by=like_count&order_by=desc&limit=${limit}`)
     } else {
-      return api.get(`/yts/api/v2/list_movies.json?sort_by=${sb}&order_by=desc&limit=${limit}`)
+      return api.get(`/api/v2/list_movies.json?sort_by=${sb}&order_by=desc&limit=${limit}`)
     }
 }
 
